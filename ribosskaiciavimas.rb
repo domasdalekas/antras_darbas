f9f202cc39a5865b686670dcdfc353b35102e0f3
domasdalekas <48487114+domasdalekas@users.noreply.github.com>
2020-10-01 16:31:21 +0300

    prideti komentarai

--- ribosskaiciavimas.rb
+++ ribosskaiciavimas.rb
@@ -1,15 +1,15 @@
 skaicius_1 = 0
 skaicius_2 = 0
 puts 'Iveskite skaiciu'
-skaicius_1 = gets.chomp.to_i
+skaicius_1 = gets.chomp.to_i # pavercia skaiciu i int
 skaicius_2 = skaicius_1 % 10
 
-while skaicius_1 >= 10
+while skaicius_1 >= 10 # apvercia skaiciu
   skaicius_1 = skaicius_1 / 10
 end
 puts "Pirmas skaicius #{skaicius_1}"
 puts "Paskutinis skaicius #{skaicius_2}"
-if(skaicius_1 == skaicius_2)
+if(skaicius_1 == skaicius_2) # palygina ar pirmas ir paskutinis skaicius sutampa
   puts 'sutampa'
 
 else puts 'Nesutampa'
