aab618f26c82d79eac21a85e10b7d45b1b45bbea
domasdalekas <48487114+domasdalekas@users.noreply.github.com>
2020-10-12 13:57:43 +0300

    Pritaikyta treciai uzduociai
    OOP
    Panaudotas rubocop

--- ribosskaiciavimas.rb
+++ ribosskaiciavimas.rb
@@ -1,16 +1,22 @@
-skaicius_1 = 0
-skaicius_2 = 0
-puts 'Iveskite skaiciu'
-skaicius_1 = gets.chomp.to_i # pavercia skaiciu i int
-skaicius_2 = skaicius_1 % 10
+class SkaiciuTikrinimas
+  def initialize
+    puts 'Iveskite skaiciu'
+    @Sk1 =gets.chomp.to_i
+    @Sk2
+  end
 
-while skaicius_1 >= 10 # apvercia skaiciu
-  skaicius_1 = skaicius_1 / 10
+  def last_digit
+    @Sk2 = @Sk1 % 10
+    @Sk1 = @Sk1 / 10 while @Sk1 >= 10
+    puts "Pirmas skaicius #{@Sk1}"
+    puts "Paskutinis skaicius #{@Sk2}"
+    if @Sk1 == @Sk2 # palygina ar pirmas ir paskutinis skaicius sutampa
+      puts 'sutampa'
+    else puts 'Nesutampa'
+    end
+  end 
 end
-puts "Pirmas skaicius #{skaicius_1}"
-puts "Paskutinis skaicius #{skaicius_2}"
-if(skaicius_1 == skaicius_2) # palygina ar pirmas ir paskutinis skaicius sutampa
-  puts 'sutampa'
 
-else puts 'Nesutampa'
-end
+
+skaiciavimas=SkaiciuTikrinimas.new()
+skaiciavimas.last_digit()
